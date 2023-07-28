62e804e5b1aa74ad29108b8540f2e5944f8529a7
DegaLaman <me@degalaman.com>
2023-07-27 20:29:17 -0500

    Eval should be done now, just needs tuning

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -7,288 +7,191 @@ using System;
 
 // Before Evaluation, 93 tokens
 // After PST, 344 tokens, up 251 tokens
-// After Full Eval, 541 tokes, up 197 tokens
+// After Full Eval, 541 tokes, up 197 token
+// After bitboard reversal and cleanup, 454 tokens, down 87 tokens
+// After adding taper eval to material, 455 tokens, up 1 token?!
 public class MyBot : IChessBot
 {
-    public ulong[,,,] PST = new ulong[2,2,6,8] // Opening/Ending, Color, PieceType, Scoreboard
+    public ulong[,,] PST = new ulong[2, 6, 8] // Opening/Ending, PieceType, Scoreboard
     {
         {
-            {
-                { // Opening/Midgame, White, Pawn, Scoreboards
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000
-                },
-                { // Opening/Midgame, White, Knight, Scoreboards
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000
-                },
-                { // Opening/Midgame, White, Bishop, Scoreboards
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000
-                },
-                { // Opening/Midgame, White, Rook, Scoreboards
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000
-                },
-                { // Opening/Midgame, White, Queen, Scoreboards
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000
-                },
-                { // Opening/Midgame, White, King, Scoreboards
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000
-                }
+            { // Opening/Midgame, White, Pawn, Scoreboards
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000
             },
-            {
-                { // Opening/Midgame, Black, Pawn, Scoreboards
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000
-                },
-                { // Opening/Midgame, Black, Knight, Scoreboards
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000
-                },
-                { // Opening/Midgame, Black, Bishop, Scoreboards
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000
-                },
-                { // Opening/Midgame, Black, Rook, Scoreboards
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000
-                },
-                { // Opening/Midgame, Black, Queen, Scoreboards
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000
-                },
-                { // Opening/Midgame, Black, King, Scoreboards
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000
-                }
+            { // Opening/Midgame, White, Knight, Scoreboards
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000
+            },
+            { // Opening/Midgame, White, Bishop, Scoreboards
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000
+            },
+            { // Opening/Midgame, White, Rook, Scoreboards
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000
+            },
+            { // Opening/Midgame, White, Queen, Scoreboards
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000
+            },
+            { // Opening/Midgame, White, King, Scoreboards
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000
             }
         },
         {
-            {
-                { // Ending, White, Pawn, Scoreboards
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000
-                },
-                { // Ending, White, Knight, Scoreboards
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000
-                },
-                { // Ending, White, Bishop, Scoreboards
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000
-                },
-                { // Ending, White, Rook, Scoreboards
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000
-                },
-                { // Ending, White, Queen, Scoreboards
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000
-                },
-                { // Ending, White, King, Scoreboards
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000
-                }
+            { // Ending, White, Pawn, Scoreboards
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000
             },
-            {
-                { // Ending, Black, Pawn, Scoreboards
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000
-                },
-                { // Ending, Black, Knight, Scoreboards
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000
-                },
-                { // Ending, Black, Bishop, Scoreboards
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000
-                },
-                { // Ending, Black, Rook, Scoreboards
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000
-                },
-                { // Ending, Black, Queen, Scoreboards
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000
-                },
-                { // Ending, Black, King, Scoreboards
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000,
-                    0x0000000000000000
-                }
+            { // Ending, White, Knight, Scoreboards
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000
+            },
+            { // Ending, White, Bishop, Scoreboards
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000
+            },
+            { // Ending, White, Rook, Scoreboards
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000
+            },
+            { // Ending, White, Queen, Scoreboards
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000
+            },
+            { // Ending, White, King, Scoreboards
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000,
+                0x0000000000000000
             }
         }
     };
 
-    public int[] pieceValue = { 100, 300, 300, 500, 900, 2000 };
+    public int[,] pieceValue = new int[2, 6]
+    {
+        { 100, 300, 300, 500, 900, 0 },
+        { 100, 300, 300, 500, 900, 0 }
+    };
     public int[] piecePhase = { 0, 1, 1, 2, 4, 0 };
 
     public int Evaluate(Board board)
     {
-        int openingScore = 0, endingScore = 0, materialScore = 0, pieceCount, phase = 0, pieceType;
-        ulong pieceBitboard;
+        int openingScore = 0,
+            endingScore = 0,
+            pieceCount,
+            phase = 0,
+            pieceType;
+        ulong pieceBitboard,
+            reversedBitboard;
 
-        for (int piece = 0; piece < 12; piece++) {
+        for (int piece = 0; piece < 12; piece++)
+        {
             pieceType = piece >> 1;
-            pieceBitboard = board.GetPieceBitboard((PieceType) (pieceType + 1), (piece & 1) == 0);
+            pieceBitboard = board.GetPieceBitboard((PieceType)(pieceType + 1), (piece & 1) == 0);
+            if ((piece & 1) != 0)
+            {
+                reversedBitboard = 0;
+                while (pieceBitboard > 0)
+                {
+                    reversedBitboard = reversedBitboard << 8 | (pieceBitboard & 0xFF);
+                    pieceBitboard >>= 1;
+                }
+                pieceBitboard = reversedBitboard;
+            }
             pieceCount = BitboardHelper.GetNumberOfSetBits(pieceBitboard);
-            openingScore -= pieceCount << 7;
-            endingScore -= pieceCount << 7;
-            materialScore += pieceCount*pieceValue[pieceType]*((piece & 1) == 0 ? 1 : -1);
-            phase += piecePhase[pieceType]*pieceCount;
-            for (int scoreboardIndex = 0; scoreboardIndex < 8; scoreboardIndex++) {
-                openingScore += (1 << scoreboardIndex)*BitboardHelper.GetNumberOfSetBits(PST[0,(piece & 1),pieceType,scoreboardIndex] & pieceBitboard);
-                endingScore += (1 << scoreboardIndex)*BitboardHelper.GetNumberOfSetBits(PST[1,(piece & 1),pieceType,scoreboardIndex] & pieceBitboard);
+            openingScore += pieceCount * (pieceValue[0, pieceType] - 128);
+            endingScore += pieceCount * (pieceValue[1, pieceType] - 128);
+            phase += piecePhase[pieceType] * pieceCount;
+            for (int scoreboardIndex = 0; scoreboardIndex < 8; scoreboardIndex++)
+            {
+                openingScore +=
+                    (1 << scoreboardIndex)
+                    * BitboardHelper.GetNumberOfSetBits(
+                        PST[0, pieceType, scoreboardIndex] & pieceBitboard
+                    );
+                endingScore +=
+                    (1 << scoreboardIndex)
+                    * BitboardHelper.GetNumberOfSetBits(
+                        PST[1, pieceType, scoreboardIndex] & pieceBitboard
+                    );
             }
         }
         phase = phase > 24 ? 24 : phase;
-        return ((openingScore * phase + endingScore * (24 - phase))/24 + materialScore) * (board.IsWhiteToMove ? 1: -1);
+        return ((openingScore * phase + endingScore * (24 - phase)) / 24)
+            * (board.IsWhiteToMove ? 1 : -1);
     }
 
     public Move Think(Board board, Timer timer)
