fe76b0d72d0ece0d04687ffb3a28150615d1de3c
DegaLaman <me@degalaman.com>
2023-08-27 19:39:34 -0500

    Adjusted some BetterUI things

--- Chess-Challenge/src/Framework/Application/UI/MenuUI.cs
+++ Chess-Challenge/src/Framework/Application/UI/MenuUI.cs
@@ -9,7 +9,7 @@ namespace ChessChallenge.Application
     {
         public static void DrawButtons(ChallengeController controller)
         {
-            Vector2 buttonPos = UIHelper.Scale(new Vector2(260, 210));
+            Vector2 buttonPos = UIHelper.Scale(new Vector2(260, 100));
             Vector2 buttonSize = UIHelper.Scale(new Vector2(260, 55));
             float spacing = buttonSize.Y * 1.2f;
             float breakSpacing = spacing * 0.6f;
@@ -75,7 +75,7 @@ namespace ChessChallenge.Application
 
             if(NextButtonInRow("Smallerer Window", ref buttonPos, spacing,  buttonSize)) 
             {
-                Program.SetWindowsSize(Settings.ScreenSizeXS);
+                Program.SetWindowSize(Settings.ScreenSizeXS);
             }
             
             if (NextButtonInRow("Exit (ESC)", ref buttonPos, spacing, buttonSize))
